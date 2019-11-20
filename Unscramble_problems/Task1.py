9d98a1b8db61d023cc297cae53898a3fca102931
Sree Nagarajugari <chaitunagarajugari@gmail.com>
2019-11-20 08:26:54 -0800

    complete 3 tasks of project1.

---
+++ Unscramble_problems/Task1.py
@@ -0,0 +1,34 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+
+def getTelephoneNumbers(data, index, telephone_numbers):
+    for i in data:
+        telephone_numbers.append(i[index])
+    return telephone_numbers
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+    telephone_numbers = []
+    getTelephoneNumbers(texts, 0, telephone_numbers)
+    getTelephoneNumbers(texts, 1, telephone_numbers)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+    getTelephoneNumbers(calls, 0, telephone_numbers)
+    getTelephoneNumbers(calls, 1, telephone_numbers)
+
+print("There are " + str(len(set(telephone_numbers))) +
+      " different telephone numbers in the records.")
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
---
+++ Unscramble_problems/Task2.py
@@ -0,0 +1,51 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+
+
+def get_telephone_numbers(calls, telephone_numbers, index):
+    for i in calls:
+        telephone_numbers.append(i[index])
+    return telephone_numbers
+
+def get_call_details(telephone_numbers, calls):
+    for i in telephone_numbers:
+        call_duration = 0
+        for j in calls:
+            if i in j:
+                call_duration += int(j[3])
+        call_details.append((i, call_duration))
+    return call_details
+
+def get_longest_call(call_details):
+    longest_call = 0
+    for i in call_details:
+        if i[1] > longest_call:
+            longest_call = i[1]
+            telephone_number = i[0]
+    return telephone_number, longest_call
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+    telephone_numbers = []
+    call_details = []
+    get_telephone_numbers(calls, telephone_numbers, 0)
+    get_telephone_numbers(calls, telephone_numbers, 1)
+    telephone_numbers = set(telephone_numbers)
+    get_call_details(telephone_numbers, calls)
+    telephone_number, longest_call = get_longest_call(call_details)
+
+print(telephone_number + " spent the longest time, " + str(longest_call) + " seconds, on the phone during September 2016.")
+
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
