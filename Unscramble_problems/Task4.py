1188df6be4a7ff129a1c6d00f215c8ffaf5435f6
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2019-11-22 20:38:07 -0800

    refactor Task4.py

--- Unscramble_problems/Task4.py
+++ Unscramble_problems/Task4.py
@@ -4,19 +4,41 @@ It's ok if you don't understand how to read files.
 """
 import csv
 
+
+def get_text_telephone_numbers(texts):
+    text_telephone_numbers = []
+    for i in texts:
+        text_telephone_numbers.append(i[0])
+        text_telephone_numbers.append(i[1])
+    return set(text_telephone_numbers)
+
+
+def get_telemarketers(calls):
+    answering_numbers = []
+    incoming_numbers = []
+    for i in calls:
+        answering_numbers.append(i[1])
+        incoming_numbers.append(i[0])
+    answering_numbers = set(answering_numbers)
+    incoming_numbers = set(incoming_numbers)
+    telemarketers = list(incoming_numbers -
+                         answering_numbers - text_telephone_numbers)
+    telemarketers.sort()
+    return telemarketers
+
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+    text_telephone_numbers = get_text_telephone_numbers(texts)
+
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-    telemarketers = []
-    for i in calls:
-        if i[0].startswith('140'):
-            telemarketers.append(i[0])
-    telemarketers = list(set(telemarketers))
-    telemarketers.sort()
+    telemarketers = get_telemarketers(calls)
     print("These numbers could be telemarketers: ")
     for i in telemarketers:
         print(i)
-
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
